9f0779881111bdf51fa1f45cab569d54a2d868a5
wgenia <wgenia@gmail.com>
2022-08-17 21:00:01 -0300

    upload 20220817

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -13,12 +25,23 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     RedesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectosComponent,
+    FooterComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RoundProgressModule],
+    NgCircleProgressModule.forRoot()
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
