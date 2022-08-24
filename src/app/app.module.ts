2372b8ab90b0109fb19d997c98211bbec80d96cc
wgenia <wgenia@gmail.com>
2022-08-24 19:19:06 -0300

    upload 24/08/22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 
@@ -43,7 +44,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     [RoundProgressModule],
     NgCircleProgressModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
    
   ],
   providers: [],
